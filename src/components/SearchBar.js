510b895ef9dea25baf4a57424487ba32e792fca0
Dominik Targosz <dominik.targosz@protonmaill.com>
2020-11-03 08:22:25 +0100

    Added filter logic for ships

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchPhrase } from "../redux/actionCreators";
 
 const SearchBar = () => {
+  const dispatch = useDispatch();
+  const searchPhrase = useSelector((state) => state.searchPhrase);
+
+  const handleChange = (e) => {
+    dispatch(setSearchPhrase(e.target.value));
+  };
+
   return (
     <input
       className="outline-none w-1/5 border-2 rounded-full py-2 px-6 ml-8"
       placeholder="Search"
+      value={searchPhrase}
+      onChange={handleChange}
     />
   );
 };
